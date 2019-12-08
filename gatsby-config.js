27bc5fc1a86a8525b83ccf1101adb3edeafeeeb1
Kyle Floros <kylefloros@gmail.com>
2019-12-07 20:31:30 -0600

    fixed icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,37 +14,38 @@ module.exports = {
         short_name: `Kyle's Stuff`,
         description: `Kyle Floros's web dev projects and notes`,
         start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
+        background_color: `#fff`,
+        theme_color: `#3f3f37`,
         display: `standalone`,
         lang: `en`,
+        icon: `src/images/icon.png`,
         icons: [
           {
-            src: `/favicons/android-chrome-192x192.png`,
+            src: `images/android-chrome-192x192.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
           {
-            src: `/favicons/android-chrome-512x512.png`,
+            src: `images/android-chrome-512x512.png`,
             sizes: `512x512`,
             type: `image/png`,
           },
           {
-            src: `/favicons/apple-touch-icon.png`,
+            src: `images/apple-touch-icon.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
           {
-            src: `/favicons/favicon-16x16.png`,
+            src: `images/favicon-16x16.png`,
             sizes: `16x16`,
             type: `image/png`,
           },
           {
-            src: `/favicons/favicon-32x32-icon.png`,
+            src: `images/favicon-32x32-icon.png`,
             sizes: `32x32`,
             type: `image/png`,
-          }
-        ]
+          },
+        ],
       },
     },
     `gatsby-plugin-mdx`,
