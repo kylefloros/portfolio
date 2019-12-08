da3531fb71cfcd0aecdf2213a001ef8828665ad9
Kyle Floros <kylefloros@gmail.com>
2019-12-07 20:17:09 -0600

    manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,10 +8,43 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `projects`,
-        path: `src/projects`,
+        name: `Kyle's Stuff`,
+        short_name: `Kyle's Stuff`,
+        description: `Kyle Floros's web dev projects and notes`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        lang: `en`,
+        icons: [
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/apple-touch-icon.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/favicon-16x16.png`,
+            sizes: `16x16`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/favicon-32x32-icon.png`,
+            sizes: `32x32`,
+            type: `image/png`,
+          }
+        ]
       },
     },
     `gatsby-plugin-mdx`,
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql, useStaticQuery, Link } from "gatsby"
+import { Link } from "gatsby"
 
 export default () => (
     <div className="m-8">
